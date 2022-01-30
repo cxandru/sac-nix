c8f7ab98cca5d6a628eb15c3dac71a42b6ccdfff
Hans-Nikolai Viessmann <hans@viess.mn>
2022-01-30 16:08:27 +0100

    add shim to give us a top-level default.nix

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-compat }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       sacVCs = {
